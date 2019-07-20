0b59647728f1001e2e7122c2f25e00e71d587deb
Marcio Nitao <marcionitao@gmail.com>
2019-07-20 19:48:07 +0200

    add list in navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -19,7 +19,21 @@
       </v-toolbar>
 
       <v-navigation-drawer app v-model="drawer" class="indigo" temporary dark>
-        <p>Test</p>
+        
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to='link.route'>
+          
+            <v-list-tile-action>
+              <v-icon class="white--text">{{ link.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title class="white--text">{{ link.text }}</v-list-tile-title>
+            </v-list-tile-content>
+
+          </v-list-tile>
+        </v-list>
+
       </v-navigation-drawer>
 
     </nav>
@@ -31,6 +45,12 @@
     data() {
       return {
         drawer: false,
+        links: [
+          { icon: 'dashboard', text: 'Dashboard', route: '/'},
+          { icon: 'folder', text: 'My Projects', route: '/projects'},
+          { icon: 'person', text: 'Team', route: '/team'},
+          { icon: 'info', text: 'About', route: '/about'},
+        ]
       }
     },
         

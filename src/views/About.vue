94064f7f038ff63bfdc4ee7dd684a0d99d655337
Marcio Nitao <marcionitao@gmail.com>
2019-07-21 19:03:19 +0200

    define template for dashboard page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1 class="subheading grey--text">This is an about page</h1>
+
+     <v-container class="my-5">
+      
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea inventore deserunt incidunt sint deleniti, nobis dignissimos porro quas laborum fugiat similique placeat quia fuga animi, nihil delectus asperiores molestiae sunt.</p>
+      
+    </v-container>
   </div>
 </template>
 
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,36 @@
 <template>
  <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
+
+     <v-container class="my-5">
+      
+      <v-card flat class="pa-3">
+        <v-layout row wrap>
+          
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>Create a new website</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+              <div>Marcio</div>
+          </v-flex>
+
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+              <div>1st Jan 2019</div>
+          </v-flex>
+
+          <v-flex xs2 sm4 md2>
+            <div class="caption grey--text">Status</div>
+              <div>ongoing</div>
+          </v-flex>
+
+        </v-layout>
+      </v-card>
+      
+    </v-container>
   </div>
 </template>
 

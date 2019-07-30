c05eb9396ee4dd8526f8d1acd7d9a4a4e8b09c3f
Marcio Nitao <marcionitao@gmail.com>
2019-07-30 18:41:05 +0200

    custom projects page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,9 +4,9 @@
 
         <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
 
-        <v-toolbar-title class="text-uppercase grey--text">
+        <v-toolbar-title class="text-uppercase">
           <span class="font-weight-light">Mein</span>
-          <span>Dashboard</span>
+          <span class="dashColor">Dashboard</span>
         </v-toolbar-title>
 
         <v-spacer></v-spacer>
@@ -66,6 +66,9 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style>
+  .dashColor {
+    color: #FF00FF;
+  }
 
 </style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -4,7 +4,17 @@
 
     <v-container class="my-5">
 
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea inventore deserunt incidunt sint deleniti, nobis dignissimos porro quas laborum fugiat similique placeat quia fuga animi, nihil delectus asperiores molestiae sunt.</p>
+      <v-expansion-panel>
+        <v-expansion-panel-content v-for="project in myProjects" :key="project.title">
+          <div slot="header">{{ project.title }}</div>
+          <v-card>
+            <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{ project.due }}</div>
+              <div>{{ project.content }}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
 
     </v-container>
   </div>
@@ -14,9 +24,21 @@
 export default {
   data () {
     return {
-
+       projects: [
+        { title: 'Design a new Website', person: 'Marcio', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea inventore deserunt incidunt sint deleniti, nobis dignissimos porro quas laborum fugiat similique placeat quia fuga animi, nihil delectus asperiores molestiae sunt.'},
+        { title: 'Create new banner', person: 'Juca', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea inventore deserunt incidunt sint deleniti, nobis dignissimos porro quas laborum fugiat similique placeat quia fuga animi, nihil delectus asperiores molestiae sunt.'},
+        { title: 'Send emails', person: 'Ze', due: '20th Dez 2018', status: 'complete', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea inventore deserunt incidunt sint deleniti, nobis dignissimos porro quas laborum fugiat similique placeat quia fuga animi, nihil delectus asperiores molestiae sunt.'},
+        { title: 'Create Community Forum', person: 'Maria', due: '20th Out 2018', status: 'overdue', content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea inventore deserunt incidunt sint deleniti, nobis dignissimos porro quas laborum fugiat similique placeat quia fuga animi, nihil delectus asperiores molestiae sunt.'}
+      ]
     }
   },
+  computed: {
+    myProjects() {
+      return this.projects.filter(project => {
+        return project.person === 'Marcio';
+      })
+    }
+  }
 }
 </script>
 

477e4a32710cae66cdb57ad7c46e2a33ad5a539c
Marcio Nitao <marcionitao@gmail.com>
2019-07-31 00:18:35 +0200

    add date picker

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -12,6 +12,21 @@
         <v-card-title>
             <h2>Add new Project</h2>
         </v-card-title>
+        <v-card-text>
+            <v-form class="px-3">
+                <v-text-field name="title" label="Title" v-model="title" prepend-icon="folder"></v-text-field>
+                <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+
+                <v-menu>
+                  <v-text-field :value="formattedDate" label="Due date" slot="activator" prepend-icon="date_range"></v-text-field>
+                  <v-date-picker v-model="due"></v-date-picker>
+                </v-menu>
+
+                <v-spacer></v-spacer>
+
+                <v-btn flat color="success mx-0 mt-3" @click="submit">Add project</v-btn>
+            </v-form>
+        </v-card-text>
 
       </v-card>
     </v-dialog>
@@ -19,15 +34,31 @@
 </template>
 
 <script>
-    export default {
-        name: 'Popup',
-        data() {
-            return {
-                
-            }
-        },
-        
-    }
+  import format from 'date-fns/format'
+
+  export default {
+    name: 'Popup',
+    data() {
+      return {
+        title: '',
+        content: '',
+        due: null
+      }
+    },
+    methods: {
+      submit() {
+        // eslint-disable-next-line no-console
+        console.log(this.title, this.content)
+      }
+    },
+    computed: {
+      // eslint-disable-next-line vue/return-in-computed-property
+      formattedDate() {
+        return this.due ? format(this.due, 'DD MMM YYYY') : ''
+      }
+    },
+      
+  }
 </script>
 
 <style lang="scss" scoped>
